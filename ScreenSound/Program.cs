2fd300fd732abcead0b93fa4b609762ea8d0f3bf
Germano Argenta <germano.a@envelcontabilidade.com.br>
2023-08-09 15:57:54 -0300

    primeiro commit

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+Musica musica1 = new Musica();
+musica1.Nome = "HeavyDirtySoul";
+musica1.Artista = "Twenty One Pilots";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.DescricaoResumida);
+
+
+Musica musica2 = new Musica();
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 217;
+musica2.Disponivel= false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+
